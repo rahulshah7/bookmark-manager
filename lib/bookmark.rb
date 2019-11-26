2d0631cb4548f42a59fcadb61c1e13fc538f41a3
Rahul Shah <rahulshah7@outlook.com>
2019-11-26 16:44:47 +0000

    Connect to PostGresSQL from Ruby

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      { name: 'Google', url: 'https://www.google.com' },
-      { name: 'Bing', url: 'https://www.bing.com' },
-      { name: 'Facebook', url: 'https://www.facebook.com' }
-    ]
+    # [
+    #   { name: 'Google', url: 'https://www.google.com' },
+    #   { name: 'Bing', url: 'https://www.bing.com' },
+    #   { name: 'Facebook', url: 'https://www.facebook.com' }
+    # ]
+    bookmarks = []
+    conn = PG.connect(dbname: 'bookmark_manager')
+    conn.exec('SELECT * FROM bookmarks') do |result|
+      result.each do |row|
+        bookmarks << { name: row.values_at('name').first, url: row.values_at('url').first }
+      end
+    end
+    bookmarks
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,12 +3,10 @@
 describe Bookmark do
   describe '.all' do
     it 'returns a list of bookmarks with a name' do
-      expect(described_class.all.map { |el| el[:name] }).to eq %w[Google Bing Facebook]
+      expect(described_class.all.map { |el| el[:name] }).to start_with('Google', 'Bing', 'Facebook')
     end
     it 'returns a list of bookmarks with a url' do
-      expect(described_class.all.map { |el| el[:url] }).to eq ['https://www.google.com',
-                                                               'https://www.bing.com',
-                                                               'https://www.facebook.com']
+      expect(described_class.all.map { |el| el[:url] }).to start_with('https://www.google.com', 'https://www.bing.com', 'https://www.facebook.com')
     end
   end
 end
